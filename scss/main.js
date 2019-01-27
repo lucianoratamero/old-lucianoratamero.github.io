ec13cc440baaa0bb4a04392b49173191ee3ebd4a
Luciano Ratamero <luciano@ratamero.com>
2019-01-27 23:28:17 +0000

    Updates

--- scss/main.js
+++ scss/main.js
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38613" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39379" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
